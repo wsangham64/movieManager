d01d9a172ae8dd3cc6dbc580aab45cd121fdd720
wsangham64 <wsangham64@gmail.com>
2022-08-18 00:27:11 +0530

    made a sample movie card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import NavBar from "./Components/NavBar";
+import MovieCard from "./Components/MovieCard";
+
 // import Sangham from "./Temp";
+
 function App() {
   return (
     <>
       <NavBar />
-      <h1>this is from ain</h1>
+
+      <div style={{ display: "flex" }}>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
     </>
   );
 }
